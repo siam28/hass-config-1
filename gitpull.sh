3cce068634a8251a4ea53d60fe122927c3ff3166
Paul Gifford <paul@giff.ca>
2017-01-17 15:23:05 -0700

    Shell Scripts Update

---
+++ gitpull.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /home/homeassistant/.homeassistant/
+# source /srv/homeassistant/homeassistant_venv/bin/activate
+# hass --script check_config
+
+git fetch https://github.com/pauldgifford/hass-config.git
+
+exit
--- gitupdate.sh
+++ gitupdate.sh
@@ -6,7 +6,7 @@ cd /home/homeassistant/.homeassistant
 
 git add .
 git status
-echo -n "Enter the Description for the Change: " [Minor Update]
+echo -n "Enter the Description for the Change: " [Version - Notes]
 read CHANGE_MSG
 git commit -m "${CHANGE_MSG}"
 git push origin master
